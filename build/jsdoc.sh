60bde7e7df6abebbffc27dc50533f33f2a7c7c48
Petro Pokydko <petro.pokydko@dtm.io>
2015-06-09 14:35:22 +0300

    updated jsdoc.sh

--- build/jsdoc.sh
+++ build/jsdoc.sh
@@ -4,33 +4,30 @@
 # https://code.google.com/p/jsdoc-toolkit/wiki/CommandlineOptions
 
 echo -n "Running jsdoc. "
-JSDOC_JAR="./lib/jsdoc-toolkit/jsrun.jar"
-SRC_PATH="../src"
+JSDOC_PATH="./lib/jsdoc-master/jsdoc"
+TEMPLATE="./lib/jsdoc-master/templates/haruki"
+SRC_PATH="../src/**/*.js"
 DOC_PATH="../docs"
-DOWNLOAD_URL=https://jsdoc-toolkit.googlecode.com/files/jsdoc_toolkit-2.4.0.zip
+DOWNLOAD_URL=https://codeload.github.com/jsdoc3/jsdoc/zip/master
 WGET="`which wget`"
 CURL="`which curl`"
 
 
-if [ ! -f "${JSDOC_JAR}" ]
+if [ ! -f "${JSDOC_PATH}" ]
 then
     mkdir -p lib
     rm -rf tmp && mkdir tmp && cd tmp
     if [ -n "$WGET" ]; then
         $WGET "${DOWNLOAD_URL}"
     else
-        $CURL "${DOWNLOAD_URL}" > ./jsdoc_toolkit-2.4.0.zip
+        $CURL "${DOWNLOAD_URL}" > jsdoc-master.zip
     fi
-    unzip jsdoc_toolkit-2.4.0.zip -d ../lib
-    cd ../lib && mv jsdoc_toolkit*/jsdoc-toolkit ./
-    rm -rf jsdoc_toolkit*
+    unzip jsdoc-master.zip -d ../lib
     cd ../ && rm -rf tmp
 fi
 
-java -jar "${JSDOC_JAR}" ./lib/jsdoc-toolkit/app/run.js \
-     -r=5 "${SRC_PATH}" \
-     -t=./lib/jsdoc-toolkit/templates/jsdoc \
-     -d="${DOC_PATH}" \
-     -e=utf-8 \
-     --quiet
+rm -rf ${DOC_PATH} && mkdir ${DOC_PATH}
+
+${JSDOC_PATH} ${SRC_PATH} -d ${DOC_PATH}
+
 echo "Done"
