f78a59dbfa0b47168a25dc49a95e4a6b19655444
Petro Pokydko <petro.pokydko@dtm.io>
2015-06-09 15:08:34 +0300

    logo added

--- build/jsdoc.sh
+++ build/jsdoc.sh
@@ -1,16 +1,22 @@
 #!/usr/bin/env bash
 
-# https://jsdoc-toolkit.googlecode.com/files/jsdoc_toolkit-2.4.0.zip
+# https://github.com/jsdoc3/jsdoc
 # https://code.google.com/p/jsdoc-toolkit/wiki/CommandlineOptions
+# http://google.github.io/styleguide/shell.xml
 
 echo -n "Running jsdoc. "
 JSDOC_PATH="./lib/jsdoc-master/jsdoc"
 TEMPLATE="./lib/jsdoc-master/templates/haruki"
+CSS_PATH="./lib/jsdoc-master/templates/default/static/styles/jsdoc-default.css"
 SRC_PATH="../src/**/*.js"
 DOC_PATH="../docs"
 DOWNLOAD_URL=https://codeload.github.com/jsdoc3/jsdoc/zip/master
 WGET="`which wget`"
 CURL="`which curl`"
+LOGO=".page-title {
+        background: url(http://datamart.github.io/Glize/images/glize-logo.png)
+        no-repeat left center/48px;
+        padding-left: 55px;}"
 
 
 if [ ! -f "${JSDOC_PATH}" ]
@@ -23,6 +29,7 @@ then
         $CURL "${DOWNLOAD_URL}" > jsdoc-master.zip
     fi
     unzip jsdoc-master.zip -d ../lib
+    echo ${LOGO} >> ${CSS_PATH}
     cd ../ && rm -rf tmp
 fi
 
