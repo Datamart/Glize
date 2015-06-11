9170b07a4bbe86443bf040d4dc26d26ebd46b7eb
Valentin Podkameny <valentin@sethq.com>
2015-06-11 13:19:02 +0300

    updated jsdocs

--- src/net/ServletRequest.js
+++ src/net/ServletRequest.js
@@ -3,7 +3,7 @@
  * @fileoverview Simple implementation of
  *               <code>javax.servlet.ServletRequest.</code>
  *
- * @see {@link //docs.oracle.com/javaee/5/api/javax/servlet/ServletRequest.html}
+ * @see {@link //docs.oracle.com/javaee/7/api/javax/servlet/ServletRequest.html}
  * @see {@link //google.github.io/styleguide/javascriptguide.xml}
  * @see {@link //developers.google.com/closure/compiler/docs/js-for-compiler}
  */
@@ -13,6 +13,7 @@
 /**
  * Simple implementation of <code>javax.servlet.ServletRequest</code>.
  * @requires util.StringUtils.URI
+ * @requires util.Object
  * @requires net.URL
  * @constructor
  * @see http://docs.oracle.com/javaee/5/api/javax/servlet/ServletRequest.html
@@ -20,8 +21,8 @@
 net.ServletRequest = function() {
 
   /**
-   * Returns the value of a request parameter as a string, or empty string if
-   * the parameter does not exist.
+   * Returns the value of a request parameter as a <code>string</code>, or empty
+   * <code>string</code> if the parameter does not exist.
    * @param {string} name A <code>string</code> specifying the name of the
    *     parameter.
    * @param {Element|Location|string=} opt_location Optional location object.
@@ -31,44 +32,55 @@ net.ServletRequest = function() {
   this.getParameter = function(name, opt_location) {
     /** @type {!Object.<string, string>} */
     var map = self_.getParameterMap(opt_location);
-    return map[/** @type {string} */ (name)] || '';
+    return map[name] || '';
+  };
+
+  /**
+   * @param {Element|Location|string=} opt_location Optional location object.
+   * @return {!Array.<string>} Returns an <code>Array</code> of
+   * <code>string</code> objects, each <code>string</code> containing the name
+   * of a request parameter; or an empty <code>Array</code> if the request has
+   * no parameters.
+   */
+  this.getParameterNames = function(opt_location) {
+    /** @type {!Object.<string, string>} */
+    var map = self_.getParameterMap(opt_location);
+    return util.Object.keys(map);
   };
 
   /**
    * Returns a map of the parameters of this request including parameters from
    * parsed from query string and hash.
-   * @param {Element|Location|string=} opt_location Optional location object.
+   * @param {Element|Location|net.URL|string=} opt_location Optional location.
    * @return {!Object.<string, string>} Map containing parameter names as keys
    *     and parameter values as map values.
    */
   this.getParameterMap = function(opt_location) {
-    /** @type {!Object.<string, string>} */ var map = {};
-    /** @type {!Array.<string>} */ var pairs = getParameterList_(opt_location);
-    /** @type {number} */ var index = pairs.length >>> 0;
-
-    while (index--) {
-      /** @type {!Array.<string>} */ var pair = pairs[index].split('=');
-      /** @type {string} */ var key = pair[0];
-      if (key) map[key] = util.StringUtils.URI.decode(pair[1]);
-    }
-    return map;
-  };
-
-  /**
-   * Gets list of parameters pairs parsed form <code>opt_location</code>.
-   * @param {Element|Location|net.URL|string=} opt_location Optional location.
-   * @return {!Array.<string>} Returns list of parameters pairs.
-   * @private
-   */
-  function getParameterList_(opt_location) {
     opt_location = opt_location || location;
     if ('string' == typeof opt_location) {
       opt_location = new net.URL(opt_location);
     }
 
-    return opt_location.search.substr(1).split('&').concat(
-        opt_location.hash.substr(1).split('&'));
-  }
+    /** @type {string} */ var url = opt_location.toString();
+    /** @type {!Object.<string, string>} */ var map = {};
+    /** @type {Array.<string>} */ var pairs;
+    /** @type {Array.<string>} */ var pair;
+    /** @type {number} */ var index;
+    /** @type {string} */ var key;
+
+    if (!(url in maps_)) {
+      pairs = opt_location.search.substr(1).split('&').concat(
+          opt_location.hash.substr(1).split('&'));
+      index = pairs.length >>> 0;
+      while (index--) {
+        pair = pairs[index].split('=');
+        key = pair[0];
+        if (key) map[key] = util.StringUtils.URI.decode(pair[1]);
+      }
+      maps_[url] = map;
+    }
+    return maps_[url];
+  };
 
   /**
    * The reference to current class instance.
@@ -77,4 +89,11 @@ net.ServletRequest = function() {
    * @private
    */
   var self_ = this;
+
+  /**
+   * Cache for parsed parameters maps.
+   * @type {!Object.<string, !Object<string,string>>}
+   * @private
+   */
+  var maps_ = {};
 };
