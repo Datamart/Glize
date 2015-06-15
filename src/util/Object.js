4740a95b9fdae4ae7d062a2ec4c9e1856d1d237a
Valentin Podkameny <valentin@sethq.com>
2015-06-15 12:52:19 +0300

    updated jsdocs

--- src/util/Object.js
+++ src/util/Object.js
@@ -1,6 +1,6 @@
 
 /**
- * @fileoverview Miscellaneous Object utility methods.
+ * @fileoverview Object utility methods.
  *
  * @see {@link http://google.github.io/styleguide/javascriptguide.xml}
  * @see {@link developers.google.com/closure/compiler/docs/js-for-compiler}
@@ -8,15 +8,16 @@
 
 
 /**
- * Miscellaneous Object utility methods.
+ * Object utility methods.
+ * @requires util.StringUtils.JSON
  * @namespace
  */
 util.Object = {
   /**
-   * Extends source with target options.
-   * @param {!Object} target Options map.
-   * @param {!Object} source Options map.
-   * @return {!Object} A map of key/value pairs of options.
+   * Extends <code>target</code> with <code>source</code> properties.
+   * @param {!Object} target The target object.
+   * @param {!Object} source The source object.
+   * @return {!Object} Returns reference to updated <code>target</code> object.
    */
   extend: function(target, source) {
     for (/** @type {string} */ var key in source) {
@@ -37,24 +38,27 @@ util.Object = {
    * Returns list of object keys.
    * @param {!Object} obj Target Object.
    * @return {!Array.<string>} List of target object keys.
+   * @see http://www.ecma-international.org/ecma-262/5.1/#sec-15.2.3.14
    */
   keys: function(obj) {
     /** @type {Function} */ var fn = Object.keys;
     /** @type {!Array.<string>} */ var keys = fn ? fn(obj) : [];
     /** @type {string|number} */ var key;
 
-    if (!fn)
-      for (key in obj)
+    if (!fn) {
+      for (key in obj) {
         keys.push(key);
+      }
+    }
 
-      return keys;
+    return keys;
   },
 
   /**
    * Filters object.
    * @param {!Object} obj The target object.
    * @param {!function(*): boolean} func The filter function.
-   * @return {!Object} Return filtered object.
+   * @return {!Object} Returns filtered object.
    */
   filter: function(obj, func) {
     /** @type {!Object} */ var result = {};
@@ -66,5 +70,15 @@ util.Object = {
       }
     }
     return result;
+  },
+
+  /**
+   * Returns a string representation of <code>obj</code> object.
+   * @param {!Object} obj The representing object.
+   * @return {string} Returns a string representing object.
+   * @see util.StringUtils.JSON.stringify
+   */
+  toString: function(obj) {
+    return util.StringUtils.JSON.stringify(obj);
   }
 };
