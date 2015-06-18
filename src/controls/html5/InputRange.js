2b83493f7c98283ac24fbb48f4972a1b72cb342c
Valentin Podkameny <valentin@sethq.com>
2015-06-18 13:23:15 +0300

    updated jsdocs

--- src/controls/html5/InputRange.js
+++ src/controls/html5/InputRange.js
@@ -1,47 +1,100 @@
 
 /**
- * @fileoverview Implementation of HTML5 input type="range" control.
- * @link http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml
- * @link https://developers.google.com/closure/compiler/docs/js-for-compiler
+ * @fileoverview Simple implementation of HTML5 input type="range" control.
+ *
+ * @see http://www.w3.org/TR/html-markup/input.range.html
+ * @see http://google.github.io/styleguide/javascriptguide.xml
+ * @see http://developers.google.com/closure/compiler/docs/js-for-compiler
  */
 
 
 
 /**
  * Constructor of InputRange.
- * @param {string|Element} input The input element or its ID attribute.
+ * @param {string|HTMLInputElement} input The input element or its ID attribute.
  * @constructor
- * @link http://www.w3.org/TR/html-markup/input.range.html
+ * @see http://www.w3.org/TR/html-markup/input.range.html
  */
 controls.html5.InputRange = function(input) {
+
+  /**
+   * @type {string}
+   * @const
+   */
+  var RANGE_TYPE = 'range';
+
+  /**
+   * This css class should emulate following pseudo-classes:
+   *     input[type=range]::-webkit-slider-runnable-track {}
+   *     input[type=range]::-moz-range-track {}
+   *     input[type=range]::-ms-track {}
+   * @type {string}
+   * @const
+   */
+  var RANGE_TRACK_CLASS = RANGE_TYPE + '-track';
+
+  /**
+   * This css class should emulate following pseudo-classes:
+   *     input[type=range]::-webkit-slider-thumb {}
+   *     input[type=range]::-moz-range-thumb {}
+   *     input[type=range]::-ms-thumb {}
+   * @type {string}
+   * @const
+   */
+  var RANGE_THUMB_CLASS = RANGE_TYPE + '-thumb';
+
+  /**
+   * This css class should emulate following pseudo-classes:
+   *     input[type=range]:focus::-webkit-slider-runnable-track {}
+   *     input[type=range]:focus::-webkit-slider-thumb {}
+   *     input[type=range]:focus::-moz-range-track {}
+   *     input[type=range]:focus::-moz-range-thumb {}
+   * @type {string}
+   * @const
+   */
+  var RANGE_TRACK_FOCUS_CLASS = RANGE_TRACK_CLASS + '-focus';
+
+  /**
+   * Css class for common implementation wrapper:
+   *     <span class="range-wrapper">
+   *       <input />
+   *       <div class="range-track">
+   *         <div class="range-thumb"></div>
+   *       </div>
+   *     </span>
+   * @type {string}
+   * @const
+   */
+  var RANGE_WRAPPER_CLASS = RANGE_TYPE + '-wrapper';
 
   /**
    * @private
    */
   function init_() {
-    if ('range' !== input_.type) {
+    if (RANGE_TYPE !== input_.type) {
 
       track_ = dom.createElement('DIV');
-      // input[type=range]::-webkit-slider-runnable-track {}
-      // input[type=range]::-moz-range-track {}
-      // input[type=range]::-ms-track {}
-      dom.css.setClass(track_, 'range-track');
+      dom.css.setClass(track_, RANGE_TRACK_CLASS);
       getWrapper_().appendChild(track_);
 
       thumb_ = dom.createElement('DIV');
-      // input[type=range]::-webkit-slider-thumb {}
-      // input[type=range]::-moz-range-thumb {}
-      // input[type=range]::-ms-thumb {}
-      dom.css.setClass(thumb_, 'range-thumb');
+      dom.css.setClass(thumb_, RANGE_THUMB_CLASS);
       track_.appendChild(thumb_);
 
       // TODO: Calculate left position depending on input value, min and max.
       thumb_.style.left = (track_.offsetWidth - thumb_.offsetWidth) / 2 + 'px';
 
-      dom.events.addEventListener(
-          track_, dom.events.TYPE.MOUSEDOWN, mousedown_);
-      dom.events.addEventListener(
-          thumb_, dom.events.TYPE.MOUSEDOWN, mousedown_);
+      if (maxTouchPoints_) {
+        dom.events.addEventListener(
+            track_, dom.events.TYPE.TOUCHSTART, mousedown_);
+        dom.events.addEventListener(
+            thumb_, dom.events.TYPE.TOUCHSTART, mousedown_);
+      } else {
+        dom.events.addEventListener(
+            track_, dom.events.TYPE.MOUSEDOWN, mousedown_);
+        dom.events.addEventListener(
+            thumb_, dom.events.TYPE.MOUSEDOWN, mousedown_);
+      }
     }
   }
 
@@ -52,7 +105,7 @@ controls.html5.InputRange = function(input) {
   function getWrapper_() {
     if (!wrapper_) {
       wrapper_ = dom.createElement('SPAN');
-      dom.css.setClass(wrapper_, 'range-wrapper');
+      dom.css.setClass(wrapper_, RANGE_WRAPPER_CLASS);
       input_.parentNode.insertBefore(wrapper_, input_);
       wrapper_.appendChild(input_);
     }
@@ -65,11 +118,18 @@ controls.html5.InputRange = function(input) {
    */
   function mousedown_(e) {
     if (dom.events.getEventTarget(e) == thumb_) {
-      dom.events.addEventListener(
-          dom.document, dom.events.TYPE.MOUSEMOVE, mousemove_);
-      dom.events.addEventListener(
-          dom.document, dom.events.TYPE.MOUSEUP, mouseup_);
-      dom.css.addClass(track_, 'range-track-focus');
+      if (maxTouchPoints_) {
+        dom.events.addEventListener(
+            dom.document, dom.events.TYPE.TOUCHMOVE, mousemove_);
+        dom.events.addEventListener(
+            dom.document, dom.events.TYPE.TOUCHEND, mouseup_);
+      } else {
+        dom.events.addEventListener(
+            dom.document, dom.events.TYPE.MOUSEMOVE, mousemove_);
+        dom.events.addEventListener(
+            dom.document, dom.events.TYPE.MOUSEUP, mouseup_);
+      }
+      dom.css.addClass(track_, RANGE_TRACK_FOCUS_CLASS);
     }
     mousemove_(e);
   }
@@ -79,11 +139,18 @@ controls.html5.InputRange = function(input) {
    * @private
    */
   function mouseup_(e) {
-    dom.events.removeEventListener(
-        dom.document, dom.events.TYPE.MOUSEMOVE, mousemove_);
-    dom.events.removeEventListener(
-        dom.document, dom.events.TYPE.MOUSEUP, mouseup_);
-    dom.css.removeClass(track_, 'range-track-focus');
+    if (maxTouchPoints_) {
+      dom.events.removeEventListener(
+          dom.document, dom.events.TYPE.TOUCHMOVE, mousemove_);
+      dom.events.removeEventListener(
+          dom.document, dom.events.TYPE.TOUCHEND, mouseup_);
+    } else {
+      dom.events.removeEventListener(
+          dom.document, dom.events.TYPE.MOUSEMOVE, mousemove_);
+      dom.events.removeEventListener(
+          dom.document, dom.events.TYPE.MOUSEUP, mouseup_);
+    }
+    dom.css.removeClass(track_, RANGE_TRACK_FOCUS_CLASS);
   }
 
   /**
@@ -94,7 +161,8 @@ controls.html5.InputRange = function(input) {
     e = dom.events.getEvent(e);
     /** @type {Object} */ var rect = dom.getBoundingClientRect(track_);
     /** @type {number} */ var margin = thumb_.offsetWidth / 2;
-    /** @type {number} */ var x = e.clientX;
+    /** @type {number} */ var x = e['changedTouches'] ?
+        e['changedTouches'][0].clientX : e.clientX;
     /** @type {number} */ var value;
 
     if (x >= rect['left'] + margin && x <= rect['right'] - margin) {
@@ -103,7 +171,7 @@ controls.html5.InputRange = function(input) {
       // TODO: Use step attribute.
       value = +(input_.value || 0);
       if (value >= min_ && value <= max_) {
-        input_.value = value;
+        input_.value = '' + value;
         dispatchEvents_();
       }
     }
@@ -114,14 +182,13 @@ controls.html5.InputRange = function(input) {
   /**
    * Dispatches input events.
    * @return {boolean} Returns <code>true</code> if all events are
-   * dispatched successfully.
+   *     dispatched successfully.
    * @private
    */
   function dispatchEvents_() {
     /** @type {boolean} */
-    var result = dom.events.dispatchEvent(input_, 'input');
-    result = dom.events.dispatchEvent(input_, dom.events.TYPE.CHANGE) && result;
-    return result;
+    var result = dom.events.dispatchEvent(input_, dom.events.TYPE.INPUT);
+    return dom.events.dispatchEvent(input_, dom.events.TYPE.CHANGE) && result;
   }
 
   /**
@@ -147,10 +214,11 @@ controls.html5.InputRange = function(input) {
 
   /**
    * The reference to input element.
-   * @type {Element}
+   * @type {HTMLInputElement}
    * @private
    */
-  var input_ = 'string' === typeof input ? dom.getElementById(input) : input;
+  var input_ = 'string' === typeof input ?
+      /** @type {HTMLInputElement} */ (dom.getElementById(input)) : input;
 
   /**
    * The expected lower bound for the input value.
@@ -172,6 +240,15 @@ controls.html5.InputRange = function(input) {
    * @private
    */
   var step_ = +(input_.getAttribute('step') || 1);
+
+  /**
+   * Detects touch screen.
+   * @type {number}
+   * @private
+   */
+  var maxTouchPoints_ = +navigator['maxTouchPoints'] ||
+                        +navigator['msMaxTouchPoints'] ||
+                        +('ontouchstart' in window);
 
   init_();
 };
