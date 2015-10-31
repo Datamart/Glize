296b265ccb9bb9cc6b375a5c31c4e0d2a493a2c1
Valentin Podkameny <valentin@sethq.com>
2015-10-31 15:14:56 +0200

    Updated jsmin.

--- build/jsmin.sh
+++ build/jsmin.sh
@@ -25,9 +25,8 @@ readonly LICENSE="/* @license http://www.apache.org/licenses/LICENSE-2.0 */"
 readonly NEW_LINE=$'\n'
 
 function main() {
-  echo -n "Running closure compiler. "
-
   if [ ! -f "${JS_COMPILER_JAR}" ]; then
+      echo "Downloading closure compiler:"
       mkdir -p "${LIB}"
       rm -rf "${TMP}" && mkdir "${TMP}" && cd "${TMP}"
       if [ -n "$WGET" ]; then
@@ -35,12 +34,14 @@ function main() {
       else
           $CURL "${JS_COMPILER_URL}" > "${TMP}/${JS_COMPILER_ZIP}"
       fi
+      echo "Extracting closure compiler:"
       unzip "${TMP}/${JS_COMPILER_ZIP}" -d "${LIB}"
       cd "${CWD}" && rm -rf "${TMP}"
   fi
 
   rm -rf "${JS_COMPILED}" && touch "${JS_COMPILED}" && chmod 0666 "${JS_COMPILED}"
 
+  echo "Running closure compiler:"
   $PYTHON -c "import sys;sys.argv.pop(0);print(' --js ' + ' --js '.join(sorted(sys.argv, cmp=lambda x,y: cmp(x.lower(), y.lower()))))" `find "${JS_SOURCES}" -name "*.js" -print` |
       xargs $JAVA -jar "${JS_COMPILER_JAR}" \
         --compilation_level ADVANCED_OPTIMIZATIONS \
