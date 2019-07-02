6f1d8d0e773f73b4f128da29fb11087417662043
Valentin Podkamennyi <valentin@dtm.io>
2019-07-01 22:04:51 -0700

    Updated npmjs file.

--- build/npmjs.sh
+++ build/npmjs.sh
@@ -5,15 +5,18 @@ readonly NPM="$(which npm)"
 
 "${NPM}" init --force
 
+ls -la "${CWD}"
+
 cd "${CWD}"
-"${NPM}" install number-utils
+"${NPM}" install --save-dev number-utils
 cd "${CWD}/node_modules/number-utils"
 "${NPM}" test
 
 cd "${CWD}"
-"${NPM}" install strings-util
+"${NPM}" install --save-dev strings-util
 cd "${CWD}/node_modules/strings-util"
 "${NPM}" test
 
 cd "${CWD}"
+ls -la "${CWD}/node_modules/"
 rm -rf node_modules package-lock.json package.json
