48e0ecaccbcb6d04de09fba93b09e2e750f29b2f
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-03-25 17:35:41 -0700

    Updated assert path

--- tests/dom/index.js
+++ tests/dom/index.js
@@ -1,4 +1,4 @@
-import * as assert from 'assert/strict';
+import * as assert from 'assert';
 import {dom} from 'glize';
 
 const runTests = () => {
--- tests/locale/index.js
+++ tests/locale/index.js
@@ -1,4 +1,4 @@
-import * as assert from 'assert/strict';
+import * as assert from 'assert';
 import {locale} from 'glize';
 
 const runTests = () => {
