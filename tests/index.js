2656cee4de066391697ea4553293a372883922e5
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-03-25 19:18:39 -0700

    Rolled back tests

--- tests/index.js
+++ tests/index.js
@@ -1,9 +1,9 @@
 
 import './compressors/index.js';
-//import './dom/index.js';
-// import './formatters/index.js';
+import './dom/index.js';
+import './formatters/index.js';
 import './locale/index.js';
-// import './net/index.js';
-// import './utils/index.js';
+import './net/index.js';
+import './utils/index.js';
 
 console.log('Done');
