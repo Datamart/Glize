5e3c5b33e954dd93275a4cf7a9a40c6fee214e61
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-06-20 19:55:22 -0700

    Upated jsdoc for debouncing and throttling events

--- src/utils/events.js
+++ src/utils/events.js
@@ -8,4 +8,38 @@
  */
 
 import { debounce, throttle } from 'debouncing';
-export { debounce, throttle };
+export {
+  /**
+   * In the debouncing technique, no matter how many times the user fires the 
+   * event, the attached function will be executed only after the specified 
+   * time once the user stops firing the event.
+   * 
+   * Returns a function, that, as long as it continues to be invoked, will not
+   * be triggered. The function will be called after it stops being called for
+   * N milliseconds.
+   * @param {!Function} func The function to execute.
+   * @param {number=} timeout The timeout in milliseconds.
+   * @return {!Function} Returns a function, that, as long as it continues 
+   *     to be invoked, will not be triggered.
+   * @see https://davidwalsh.name/javascript-debounce-function
+   * @see https://github.com/Datamart/debouncing
+   * @method
+   */
+  debounce,
+
+  /**
+   * Throttling is a technique in which, no matter how many times the user 
+   * fires the event, the attached function will be executed only once in a 
+   * given time interval.
+   * 
+   * Returns a function, that, as long as it continues to be invoked, will only
+   * trigger every N milliseconds.
+   * @param {!Function} func The function to execute.
+   * @param {number=} timeout The timeout in milliseconds.
+   * @return {!Function} Returns a function, that, as long as it continues 
+   *     to be invoked, will only trigger every N milliseconds.
+   * @see https://github.com/Datamart/debouncing
+   * @method
+   */
+  throttle
+};
