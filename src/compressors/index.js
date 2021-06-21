ae12be068983e33b1d2c9f4f07bcf0da000126a9
Valentin Podkamennyi <vpodkamenyi@gmail.com>
2021-06-20 21:06:21 -0700

    Extracted lzw-compressor module from Glize library.

--- src/compressors/index.js
+++ src/compressors/index.js
@@ -6,6 +6,7 @@
  * @module glize/compressors
  */
 
+import * as lzw from 'lzw-compressor';
 
 /**
  * Enumeration of available compression types.
@@ -31,7 +32,7 @@ export const compress = (data, opt_type = TYPE.LZW) => {
   let result = '';
 
   if (TYPE.LZW === opt_type) {
-    result = LZW.compress(data);
+    result = lzw.compress(data);
   }
 
   return result;
@@ -52,74 +53,8 @@ export const decompress = (data, opt_type = TYPE.LZW) => {
   let result = '';
 
   if (TYPE.LZW === opt_type) {
-    result = LZW.decompress(data);
+    result = lzw.decompress(data);
   }
 
   return result;
-};
-
-/**
- * LZW compression utility.
- * @inner
- * @see http://en.wikipedia.org/wiki/Lempel–Ziv–Welch
- */
-const LZW = {
-  /**
-   * Encodes string using LZW algorithm.
-   * @param {string} str The input string.
-   * @return {string} Returns compressed string using LZW algorithm.
-   * @method
-   */
-  compress: (str) => {
-    const dict = {};
-    const data = str.split('');
-    const out = [];
-    let code = 256;
-    let phrase = data.shift();
-
-    while (data.length) {
-      const next = data.shift();
-      if (null != dict[phrase + next]) {
-        phrase += next;
-      } else {
-        out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-        dict[phrase + next] = code++;
-        phrase = next;
-      }
-    }
-    out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-    for (let i = 0; i < out.length; i++) {
-      out[i] = String.fromCharCode(/** @type {number} */ (out[i]));
-    }
-
-    return out.join('');
-  },
-
-  /**
-   * Decodes string encoded with LZW algorithm.
-   * @param {string} str The input string encoded with LZW algorithm.
-   * @return {string} Returns decoded string.
-   * @method
-   */
-  decompress: (str) => {
-    const dict = {};
-    const data = str.split('');
-    const out = [data.shift()];
-    let code = 256;
-    let chr = out[0];
-    let tmp = chr;
-
-    for (let i = 0; i < data.length; i++) {
-      const next = data[i].charCodeAt(0);
-      const phrase = next < 256 ? 
-          data[i] : 
-          (dict[next] ? dict[next] : (tmp + chr));
-      out.push(phrase);
-      chr = phrase.charAt(0);
-      dict[code++] = tmp + chr;
-      tmp = phrase;
-    }
-
-    return out.join('');
-  }
 };
